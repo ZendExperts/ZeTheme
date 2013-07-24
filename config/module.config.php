fa0884744c039cab23fa4f06e49e2b00f956bb2c
Alin Jurj <office@perfectweb.ro>
2013-07-24 13:41:07 +0300

    added custom path

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,7 @@
 return array(
     'ze_theme' => array(
         'default_theme' => null,
+        'custom_theme_path' => false,
         'theme_paths' => array(
             __DIR__ . '/../themes/'
         ),
